16dec12c4614bfef2a132887f8c7035ef75f9df1
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-09-13 09:24:44 +0200

    rabbitmq tut

---
+++ Send/Send.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using RabbitMQ.Client;
+
+namespace L02.Send
+{
+    class Send
+    {
+        static void Main(String[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                routingKey: "hello",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+        }
+    }    
+}
+
