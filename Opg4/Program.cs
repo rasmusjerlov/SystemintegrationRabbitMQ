92fd1dcac540e7ad3d3ff3092bb378fd480e538b
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-09-13 10:29:21 +0200

    Opgave5

---
+++ Opg4/Program.cs
@@ -0,0 +1,21 @@
+﻿// {
+//     "header": {
+//         "message_id": "MSG12345",
+//         "timestamp": "2024-09-14T10:15:00Z",
+//         "sender": "KLM",
+//         "receiver": "BluffCityAirportInfoCenter"
+//     },
+//     "body": {
+//         "flyselskab": "KLM",
+//         "flightNo": "KL4567",
+//         "planlagt_tid": {
+//             "afgang": "2024-09-14T14:30:00",
+//             "ankomst": "2024-09-14T18:45:00"
+//         },
+//         "destination": "Bluff City International Airport",
+//         "check_in": {
+//             "start_tid": "2024-09-14T12:00:00",
+//             "slut_tid": "2024-09-14T13:45:00"
+//         }
+//     }
+// }
