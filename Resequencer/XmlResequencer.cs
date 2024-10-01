c62cdbc5a94c8a298655bb2afc00212f2c925280
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-10-01 11:55:22 +0200

    No top level statements

--- Resequencer/XmlResequencer.cs
+++ Resequencer/XmlResequencer.cs
@@ -8,40 +8,41 @@ using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace MiniProjekt;
+List<dynamic> messages = new List<dynamic>();
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channelReseq = connection.CreateModel();
 
-public class XmlResequencer
+var xmlConsumer = new EventingBasicConsumer(channelReseq);
+xmlConsumer.Received += (model, ea) =>
 {
-    static void Main(String[] args)
-    {
-        List<dynamic> messages = new List<dynamic>();
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        var connection = factory.CreateConnection();
-        var channelReseq = connection.CreateModel();
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    dynamic deserializedMessage = JsonConvert.DeserializeObject(message);
+    messages.Add(deserializedMessage);
+    Console.WriteLine($" [x] Received {message}");
+};
 
-        var xmlConsumer = new EventingBasicConsumer(channelReseq);
-        xmlConsumer.Received += (model, ea) =>
-        {
-            var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            dynamic deserializedMessage = JsonConvert.DeserializeObject(message);
-            messages.Add(deserializedMessage);
-            // Console.WriteLine($" [x] Received {message}");
-        };
-        
 
-        channelReseq.BasicConsume(queue: "XmlQueue",
-            autoAck: true,
-            consumer: xmlConsumer);
-        
-        messages.Sort((x, y) => x.SequenceNumber.CompareTo(y.SequenceNumber));
-        Console.WriteLine("Sorted:");
-        foreach (var msg in messages)
-        {
-            Console.WriteLine($"Processed message with SequenceNumber: {msg.SequenceNumber}");
-        }
-        
-        Console.WriteLine("Press [enter] to exit.");
-        Console.ReadLine();
-    }
+channelReseq.BasicConsume(queue: "XmlQueue",
+    autoAck: true,
+    consumer: xmlConsumer);
+
+Thread.Sleep(5000);
+
+messages.Sort((x, y) => x.SequenceNumber.CompareTo(y.SequenceNumber));
+
+foreach (var msg in messages)
+{
+    
+    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
+    channelReseq.BasicPublish(exchange: "",
+        routingKey: "XmlQueue",
+        mandatory: false,
+        basicProperties: null,
+        body: body);
 }
+
+Console.WriteLine("Resequenced messages has been sent to the queue and have been consumed.");
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
--- Splitter/XmlSplitter.cs
+++ Splitter/XmlSplitter.cs
@@ -4,90 +4,83 @@ using System.Xml.Linq;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 
-namespace MiniProjekt;
-
-public class XmlSplitter
+try
 {
-    static void Main(String[] args)
-    {
-        try
-        {
-            string xmlFilePath = "/Users/rasmusjerlov/Library/CloudStorage/OneDrive-EFIF/Datamatiker/Projects/Rider/SystemintegrationRabbitMQ/MiniProjekt/FlightDetalisInfoResponse.xml";
-            var xmlContent = System.IO.File.ReadAllText(xmlFilePath);
-            var xDocument = XDocument.Parse(xmlContent);
+string xmlFilePath =
+"/Users/rasmusjerlov/Library/CloudStorage/OneDrive-EFIF/Datamatiker/Projects/Rider/SystemintegrationRabbitMQ/MiniProjekt/FlightDetalisInfoResponse.xml";
+var xmlContent = System.IO.File.ReadAllText(xmlFilePath);
+var xDocument = XDocument.Parse(xmlContent);
 
-            var flightDetails = xDocument.Element("FlightDetailsInfoResponse").Element("Flight");
-            var passenger = xDocument.Element("FlightDetailsInfoResponse").Element("Passenger");
-            var luggages = xDocument.Element("FlightDetailsInfoResponse").Elements("Luggage");
+var flightDetails = xDocument.Element("FlightDetailsInfoResponse").Element("Flight");
+var passenger = xDocument.Element("FlightDetailsInfoResponse").Element("Passenger");
+var luggages = xDocument.Element("FlightDetailsInfoResponse").Elements("Luggage");
 
-            var messages = new List<string>();
-            int sequenceNumber = 0;
+var messages = new List<string>();
+int sequenceNumber = 0;
 
-            // Create flight details message
-            var flightMessage = new
-            {
-                Type = "Flight",
-                Number = flightDetails.Attribute("number").Value,
-                FlightDate = flightDetails.Attribute("Flightdate").Value,
-                Origin = flightDetails.Element("Origin").Value,
-                Destination = flightDetails.Element("Destination").Value,
-                Timestamp = DateTime.Now,
-                SequenceNumber = sequenceNumber++
-            };
-            messages.Add(JsonConvert.SerializeObject(flightMessage));
+// Create flight details message
+var flightMessage = new
+{
+Type = "Flight",
+Number = flightDetails.Attribute("number").Value,
+FlightDate = flightDetails.Attribute("Flightdate").Value,
+Origin = flightDetails.Element("Origin").Value,
+Destination = flightDetails.Element("Destination").Value,
+Timestamp = DateTime.Now,
+SequenceNumber = sequenceNumber++
+};
+messages.Add(JsonConvert.SerializeObject(flightMessage));
 
-            // Create passenger message
-            var passengerMessage = new
-            {
-                Type = "Passenger",
-                ReservationNumber = passenger.Element("ReservationNumber").Value,
-                FirstName = passenger.Element("FirstName").Value,
-                LastName = passenger.Element("LastName").Value,
-                Timestamp = DateTime.Now,
-                SequenceNumber = sequenceNumber++
-            };
-            messages.Add(JsonConvert.SerializeObject(passengerMessage));
+// Create passenger message
+var passengerMessage = new
+{
+Type = "Passenger",
+ReservationNumber = passenger.Element("ReservationNumber").Value,
+FirstName = passenger.Element("FirstName").Value,
+LastName = passenger.Element("LastName").Value,
+Timestamp = DateTime.Now,
+SequenceNumber = sequenceNumber++
+};
+messages.Add(JsonConvert.SerializeObject(passengerMessage));
 
-            // Create luggage messages
-            foreach (var luggage in luggages)
-            {
-                var luggageMessage = new
-                {
-                    Type = "Luggage",
-                    Id = luggage.Element("Id").Value,
-                    Identification = luggage.Element("Identification").Value,
-                    Category = luggage.Element("Category").Value,
-                    Weight = luggage.Element("Weight").Value,
-                    SequenceNumber = sequenceNumber++
-                };
-                messages.Add(JsonConvert.SerializeObject(luggageMessage));
-            }
+// Create luggage messages
+foreach (var luggage in luggages)
+{
+var luggageMessage = new
+{
+    Type = "Luggage",
+    Id = luggage.Element("Id").Value,
+    Identification = luggage.Element("Identification").Value,
+    Category = luggage.Element("Category").Value,
+    Weight = luggage.Element("Weight").Value,
+    SequenceNumber = sequenceNumber++
+};
+messages.Add(JsonConvert.SerializeObject(luggageMessage));
+}
 
-            // Send messages to RabbitMQ
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
+// Send messages to RabbitMQ
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: "XmlQueue",
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null);
+channel.QueueDeclare(queue: "XmlQueue",
+durable: false,
+exclusive: false,
+autoDelete: false,
+arguments: null);
 
-            foreach (var message in messages)
-            {
-                var body = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish(exchange: "",
-                    routingKey: "XmlQueue",
-                    basicProperties: null,
-                    body: body);
-            }
+foreach (var message in messages)
+{
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "",
+    routingKey: "XmlQueue",
+    basicProperties: null,
+    body: body);
+}
 
-            Console.WriteLine("Messages sent to RabbitMQ.");
-        }
-        catch (NullReferenceException nr)
-        {
-            Console.WriteLine(nr.Message);
-        }
-    }
+Console.WriteLine("Messages sent to RabbitMQ.");
+}
+catch (NullReferenceException nr)
+{
+Console.WriteLine(nr.Message);
 }
